49f6988d626bd18066b79c3c04d9072df11355fe
gabmotamelo <gabrielmota6@hotmail.com>
2023-01-09 15:29:03 -0300

    Creating project

---
+++ ecommerce/src/main/java/com/ecommerce/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return addCorsMapping(registry) -> {
+//            registry.add
+//        }
+//    }
+}
